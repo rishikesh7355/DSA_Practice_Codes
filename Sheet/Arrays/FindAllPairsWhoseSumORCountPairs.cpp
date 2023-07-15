d5832ca3b2894c6ba0428e148e806ca0320fcce4
rishikesh7355 <rishiyadav280198@gmail.com>
2023-07-15 23:17:46 +0530

    workingCountPairs

--- Sheet/Arrays/FindAllPairsWhoseSumORCountPairs.cpp
+++ Sheet/Arrays/FindAllPairsWhoseSumORCountPairs.cpp
@@ -3,22 +3,35 @@
 #include<bits/stdc++.h> 
 using namespace std;
 
+/////////////Dry Run ///////////////////////
+//time complexcity : O(n)
+// space complexcity: o(1) 
+
+// n=4 , k=6
+// arr = [1 5  7 1]
+// count = 0
+// unordered mp;
+// we need to find pairs (a,b) here a= arr[i] and b = k-arr[i] with frequency
+// i = 0; b= k-arr[0] = 5; not in map so put in map = [5 -> 1] count = 0
+// i= 1;b= k-arr[1] = 1; map = [5 ->1 , 1 -> 1] count = 1
+// i= 2; b=k-arr[2] = -1 ; not in map ignore
+// i = 3 ;b= k-arr[3] = 5 = [5 ->2 , 1 ->1] count 2
 int countPairs(int arr[],int n,int k){
    unordered_map<int,int>mp;
    int count=0;
    for(int i=0;i<n;i++){
        int b = k-arr[i];
        if(mp[b]){
-           count+=mp[b];
+           count+=mp[b]; //increase frequency if already there 
        }
-       mp[arr[i]]++;
+       mp[arr[i]]++; // this has to be done always if element not in map put in map 
    }
    return count;
 }
 
 int main() {
    int arr[4];
-   int n = sizeof(arr[0])/sizeof(int);
+   int n = sizeof(arr)/sizeof(int);
    cout<<"enter elements"<<endl;
    for(int i=0;i<n;i++){
        cin>>arr[i];

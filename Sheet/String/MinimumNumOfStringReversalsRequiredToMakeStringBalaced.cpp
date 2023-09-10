63c943b7b761e05244a68212827cf75503ea5fe0
rishikesh7355 <rishiyadav280198@gmail.com>
2023-09-10 23:58:15 +0530

    StrReversalsDryRUn

---
+++ Sheet/String/MinimumNumOfStringReversalsRequiredToMakeStringBalaced.cpp
@@ -0,0 +1,15 @@
+S = "}{{}}{{{"
+Output: 3
+
+n = 8
+
+temp=0 , res=0 n%2 == 0
+if s[0] == { no 
+else if(temp==0) res++ temp++ res=1 temp =1
+s[1] == { yes  temp==2 
+s[2] == { yes temp=3 
+s[3] == } else temp==0 no else temp-- temp=2
+s[4] == } else temp==0 no else temp-- temp=1
+s[5] =={ temp++ temp=2
+s[6] == { temp++ temp=3
+s[7] == { 
